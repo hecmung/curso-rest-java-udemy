9e8242262ccec66a21967214d168c1c407b97b8d
hecmung <kenwey3018@gmail.com>
2023-06-08 20:23:12 -0600

    Product Cpntroller

---
+++ rest/src/main/java/com/curso/rest/controller/ProductController.java
@@ -0,0 +1,14 @@
+package com.curso.rest.controller;
+
+import com.curso.rest.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+}
