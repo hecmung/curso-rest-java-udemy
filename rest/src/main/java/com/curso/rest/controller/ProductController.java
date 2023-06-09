ff7df9420e3e8d33ae26d78ce22acf0688824fc0
hecmung <kenwey3018@gmail.com>
2023-06-08 21:07:54 -0600

    Product Controller finished

--- rest/src/main/java/com/curso/rest/controller/ProductController.java
+++ rest/src/main/java/com/curso/rest/controller/ProductController.java
@@ -1,9 +1,11 @@
 package com.curso.rest.controller;
 
+import com.curso.rest.model.Product;
 import com.curso.rest.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
@@ -11,4 +13,29 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+
+    @GetMapping("/{id}")
+    public Product findById(@PathVariable Long id) {
+        return productService.findById(id);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @PostMapping
+    public Product save(@RequestBody Product product) {
+        return productService.save(product);
+    }
+
+    @PutMapping
+    public Product update(@RequestBody Product product) {
+        return productService.update(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean delete(@PathVariable Long id) {
+        return productService.delete(id);
+    }
 }
