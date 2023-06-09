53ce6f4c8f38c4d8d4d2fe88b23bf3ec58d02393
hecmung <kenwey3018@gmail.com>
2023-06-08 20:11:31 -0600

    Prodcut Model

--- rest/src/main/java/com/curso/rest/model/Product.java
+++ rest/src/main/java/com/curso/rest/model/Product.java
@@ -7,11 +7,11 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Producto {
+public class Product {
 
     private Long id;
-    private ;
-    private ;
-    private ;
-    private ;
+    private String name;
+    private int amount;
+    private double price;
+
 }
